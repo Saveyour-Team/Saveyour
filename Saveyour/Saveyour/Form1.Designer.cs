daadb1edc53bf81b4ffa8110f6357788e07e9af5
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-03-01 11:28:14 -0500

    Formatted form1 to Saveyour Login box. Stylized a bit

--- Saveyour/Saveyour/Form1.Designer.cs
+++ Saveyour/Saveyour/Form1.Designer.cs
@@ -28,21 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 249);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(742, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Username";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(186, 332);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(742, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Log In";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(210, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(686, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome to Saveyour";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1114, 619);
+            this.ClientSize = new System.Drawing.Size(1114, 616);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Saveyour";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Saveyour/Saveyour/Form1.cs
+++ Saveyour/Saveyour/Form1.cs
@@ -21,5 +21,15 @@ namespace Saveyour
         {
 
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

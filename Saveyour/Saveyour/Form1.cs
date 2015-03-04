3644b246787e399c241edd16c79a8f243d0f42ab
dengitzjg <dengitzjg@gmail.com>
2015-03-03 22:53:57 -0500

    Centered forms upon creation

--- Saveyour/Saveyour/Form1.cs
+++ Saveyour/Saveyour/Form1.cs
@@ -71,8 +71,8 @@ namespace Saveyour
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {           
-            
+        {
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -85,5 +85,7 @@ namespace Saveyour
         {
 
         }
+
+
     }
 }
--- Saveyour/Saveyour/Form2.Designer.cs
+++ Saveyour/Saveyour/Form2.Designer.cs
@@ -60,6 +60,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
             this.ResumeLayout(false);
             this.PerformLayout();

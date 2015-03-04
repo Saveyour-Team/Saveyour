3a8eb2ec31227cf2f5be7256bf07f420f5d4254f
dengitzjg <dengitzjg@gmail.com>
2015-03-04 00:48:52 -0500

    Added some important comments regarding why Form2 was written the way it was

--- Saveyour/Saveyour/Form2.cs
+++ Saveyour/Saveyour/Form2.cs
@@ -25,6 +25,14 @@ namespace Saveyour
             InitializeComponent();
         }
 
+        /*
+         * A specialized constructor was created so that we can destroy the login from this new window.
+         * This may not be relevant later because right now you can think of Form1 as being the parent of Form2
+         * When Form1 is destroyed, Form2 is also destroyed. Right now it just hides Form1 and destroys it when
+         * Form2 is no longer need. 
+         * 
+         * We may want to find a way to create Form2 without making Form1 the parent.
+         * */
         public Form2(Form parent)
         {
             InitializeComponent();
@@ -34,7 +42,7 @@ namespace Saveyour
         private void button1_Click(object sender, EventArgs e)
         {
             parent.Dispose();
-            this.Dispose();
+            this.Dispose(); //disposing the parent should make sure that this doesn't run; was unsure if this was needed
         }
 
         private void label1_Click(object sender, EventArgs e)

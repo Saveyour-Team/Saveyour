8d1c238848cf36e290958c0f64afff1f27648705
dengitzjg <dengitzjg@gmail.com>
2015-03-03 20:29:23 -0500

    Created NetworkControl and moved network control elements to the new class

--- Saveyour/Saveyour/Form1.cs
+++ Saveyour/Saveyour/Form1.cs
@@ -14,15 +14,12 @@ namespace Saveyour
 {
     public partial class Form1 : Form
     {
-        static String SERVERIP = "127.0.0.1";
-        static Int32 SERVERPORT = 80;
         static Boolean userClick = true;
         static Boolean passClick = true;
 
         public Form1()
         {
             InitializeComponent();
-            //String response = Connect(SERVERIP, "Poop");
             button1.Click += new EventHandler(login_Click);
             textBox1.Click += new EventHandler(user_Click);
             textBox2.Click += new EventHandler(pass_Click);
@@ -34,7 +31,10 @@ namespace Saveyour
         {
             String username = textBox1.Text;
             String password = textBox2.Text;
-            String response = Connect(SERVERIP, username + "," + password);
+
+            //Create a network connection and connect
+            NetworkControl network = new NetworkControl();
+            String response = network.Connect(network.getIP(), username + "," + password);
             Debug.WriteLine(response); 
         }
 
@@ -75,64 +75,6 @@ namespace Saveyour
 
         }
 
-        static String Connect(String server, String message)
-        {
-            try
-            {
-                // Create a TcpClient. 
-                // Note, for this client to work you need to have a TcpServer  
-                // connected to the same address as specified by the server, port 
-                // combination.
-                
-                TcpClient client = new TcpClient(server, SERVERPORT);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing. 
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);
-
-                // Receive the TcpServer.response. 
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
-
-                return responseData;
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-                return "";
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-                return "";
-            }
-
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
-            return "";
-        }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
---
+++ Saveyour/Saveyour/NetworkControl.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Saveyour
+{
+
+    class NetworkControl
+    {
+        String SERVERIP = "127.0.0.1";
+        Int32 SERVERPORT = 80;
+
+        public String getIP()
+        {
+            return SERVERIP;
+        }
+
+        public String Connect(String server, String message)
+        {
+            try
+            {
+                // Create a TcpClient. 
+                // Note, for this client to work you need to have a TcpServer  
+                // connected to the same address as specified by the server, port 
+                // combination.
+
+                TcpClient client = new TcpClient(server, SERVERPORT);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing. 
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response. 
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+
+                return responseData;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+                return "";
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                return "";
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+            return "";
+        }
+
+    }
+}

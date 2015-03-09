efbb6ccb21c0a5f1bb774dc61813f1ab008266c1
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-03-08 21:03:18 -0400

    Small change to get client to work pre-shell

--- Saveyour/Saveyour/Program.cs
+++ Saveyour/Saveyour/Program.cs
@@ -16,7 +16,7 @@ namespace Saveyour
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Shell());
+            Application.Run(new Login());
         }
     }
 }

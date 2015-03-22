342249146912043ab7a7f0b4d8f7cfaed0587057
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-03-22 17:47:23 -0400

    Added skeletons for abstract classes.

---
+++ Saveyour/Saveyour/Module.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saveyour
+{
+    abstract class Module
+    {
+        protected int moduleID;
+
+        public Module()
+        {
+
+        }
+
+        public Boolean add()
+        {
+            return true;
+        }
+
+        public Boolean delete()
+        {
+            return true;
+        }
+
+        public Boolean update()
+        {
+            return true;
+        }
+
+        public Boolean save()
+        {
+            return true;
+        }
+
+        public Boolean load()
+        {
+            return true;
+        }
+
+        abstract public Array getDates();
+
+    }
+}

8cb5b8216db13fbbee0267c8f164f64d9249fafb
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-03-28 13:10:43 -0400

    Threaded one second disappearance for saved

--- Saveyour/Saveyour/ReadWrite.cs
+++ Saveyour/Saveyour/ReadWrite.cs
@@ -8,22 +8,22 @@ namespace Saveyour
 {
     public static class ReadWrite
     {
-        public static Boolean write(String settings)
-        {
-            String directory = @"C:\Saveyour\";
+        static String directory = @"savedFiles\"; //Saves to where the .exe is located
+        //String directory = @"C:\Saveyour\";
 
+        public static Boolean write(String settings)
+        {                                   
             bool exists = System.IO.Directory.Exists(directory);
 
             if (!exists)
                 System.IO.Directory.CreateDirectory(directory);
-
+            
             System.IO.File.WriteAllText(directory + "settings.txt", settings);
             return true;
         }
 
         public static String read()
-        {
-            String directory = @"C:\Saveyour\";
+        {           
 
             bool exists = System.IO.Directory.Exists(directory);
 
@@ -33,7 +33,7 @@ namespace Saveyour
                 return "No settings found. Please save settings before loading";
             }
 
-            string text = System.IO.File.ReadAllText(@"C:\Saveyour\settings.txt");
+            string text = System.IO.File.ReadAllText(directory + "settings.txt");
             return text;
         }
     }

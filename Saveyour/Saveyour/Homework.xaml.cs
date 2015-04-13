80aa43fe4ce8b9972fa2f1772b8900d7b90f5b6d
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-04-13 13:20:05 -0400

    Began skeleton for homework module

---
+++ Saveyour/Saveyour/Homework.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Saveyour
+{
+    /// <summary>
+    /// Interaction logic for Homework.xaml
+    /// </summary>
+    public partial class Homework : Window, Module
+    {
+        public Homework()
+        {
+            InitializeComponent();
+        }
+
+        public String moduleID()
+        {
+            return "Homework";
+        }
+
+        public Boolean update()
+        {
+            return false;
+        }
+
+        public String save()
+        {
+            return "";
+        }
+
+        public Boolean load(String data)
+        {
+            return false;
+        }
+
+        public Boolean Equals(Module other)
+        {
+            return moduleID().Equals(other.moduleID());
+        }
+    }
+}

e3f5b08f595be82db5637340493a5d6ee81d05e2
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-04-15 21:07:29 -0400

    Outlining/Documenting necessary steps to implement

--- Saveyour/Saveyour/Homework.xaml.cs
+++ Saveyour/Saveyour/Homework.xaml.cs
@@ -19,9 +19,13 @@ namespace Saveyour
     /// </summary>
     public partial class Homework : Window, Module
     {
+        private const int MAX_SUBJECTS = 7;
+        private int numSubjects = 0;
 
         GroupBox first = null;
         Window parent = null;
+
+        GroupBox[] subjects = new GroupBox[MAX_SUBJECTS];
 
         public Homework()
         {
@@ -30,7 +34,15 @@ namespace Saveyour
             first = (GroupBox)FindName("subject1");
             parent = (Window)FindName("hWindow");
 
+            //We need to load the information for the subjects here.
 
+            //After loading the information, we increase numSubjects and take the data from load to construct each GroupBox for each Grid
+
+            //Load the GroupBox, then the Grid, then each task in Task, then each date in Date
+
+            //
+            
+            
         }
 
         public String moduleID()
@@ -59,6 +71,16 @@ namespace Saveyour
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Task_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }

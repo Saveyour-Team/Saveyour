55178648bbb19e1ffe733c29faedca24991e9f98
Brendan Teich <bpteich@gmail.com>
2015-04-27 13:18:16 -0400

    Fixed an error with the ordering of days in WeeklyToDo on initialization.

--- Saveyour/Saveyour/WeeklyToDo.xaml.cs
+++ Saveyour/Saveyour/WeeklyToDo.xaml.cs
@@ -125,6 +125,7 @@ namespace Saveyour
             borders[13] = SaturdayTaskBorder;
 
             int numToday = (int)DateTime.Today.DayOfWeek;
+
             today = days[numToday];
             todayBorder = borders[numToday * 2];
             reOrderDays();
@@ -132,16 +133,14 @@ namespace Saveyour
             int j = 0;
             foreach (TextBlock day in days)
             {
-                    //numToday--;
-		         colorByWeights(j);
-                   // day.Text += " " + DateTime.Today.AddDays(numToday * -1).ToString("d");
-
+		       colorByWeights(j);
                day.Text = "";
-               int count = (j - numToday + 7) % 7;
-               day.Text = curTopDay.AddDays(count - numToday).ToString("dddd");
-               day.Text += " " + curTopDay.AddDays(count - numToday).ToString("d");
-                   j++;
+               day.Text = curTopDay.AddDays(j - numToday).ToString("dddd");
+               day.Text += " " + curTopDay.AddDays(j - numToday).ToString("d");
+               j++;
             }
+
+            reOrderDays();
           
             today.Text += " (TODAY)";
             //COLORS ARE SUBJECT TO CHANGE (someone change them if they have a good color scheme!)

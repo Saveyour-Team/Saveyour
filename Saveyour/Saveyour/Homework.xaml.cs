b97af92da91c774a343ed189ad679b1df61319b1
Terrence Yu <zhenhongyu@umass.edu>
2015-04-27 00:34:05 -0400

    Updated Homework Display as a tab rather than a list.

--- Saveyour/Saveyour/Homework.xaml.cs
+++ Saveyour/Saveyour/Homework.xaml.cs
@@ -15,6 +15,7 @@ using System.IO;
 using System.Windows.Markup;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
+using System.Xml;
 
 namespace Saveyour
 {
@@ -33,22 +34,33 @@ namespace Saveyour
         }
 
         ObservableCollection<Task> taskCollection;
+        ObservableCollection<TabItem> subjectsCollection;
+        ListView defaultListStyle;
+        String tabXAML;
+        StringReader readString;
+        XmlReader reader;
+        TabItem setTab;
+        Button addButton;
+        
         private class Subject
         {
             public FlowDocument dates, assignments;
             public TextBox subject;            
         }
         
-        private const int MAX_SUBJECTS = 5;
-        private int numSubjects = 0;
-        private int totalSubjects = 0;
+        private const int MAX_SUBJECTS = 10;
+        private int numSubjects = 1;
+        private int totalSubjects = 1;
         Subject[] subjects = new Subject[MAX_SUBJECTS];
 
         public Homework()
         {
             InitializeComponent();
-            taskCollection = new ObservableCollection<Task>();
-            taskList.ItemsSource = taskCollection;
+            subjectsCollection = new ObservableCollection<TabItem>();
+            taskList.ItemsSource = new ObservableCollection<Task>();
+            defaultListStyle = taskList;
+            tabXAML = XamlWriter.Save(taskList);
+
             //We need to load the information for the subjects here.
 
             //After loading the information, we increase numSubjects and take the data from load to construct each GroupBox for each Grid
@@ -101,14 +113,14 @@ namespace Saveyour
 
             /****** LOAD FLOW DOCUMENT *******/
 
-            FileStream xamlFile = new FileStream(@"savedFiles\test.xaml", FileMode.Open, FileAccess.Read);
+            //FileStream xamlFile = new FileStream(@"savedFiles\test.xaml", FileMode.Open, FileAccess.Read);
             // and parse the file with the XamlReader.Load method.
-            FlowDocument content = XamlReader.Load(xamlFile) as FlowDocument;
+            //FlowDocument content = XamlReader.Load(xamlFile) as FlowDocument;
             // Finally, set the Document property to the FlowDocument object that was
             // parsed from the input file.            
             //rightBox.Document = content;
 
-            xamlFile.Close();          
+            //xamlFile.Close();          
 
             return true;
         }
@@ -135,110 +147,43 @@ namespace Saveyour
 
         private void addSubjectButton_Click(object sender, RoutedEventArgs e)
         {
-            if (totalSubjects == MAX_SUBJECTS)
-            {
-                Console.WriteLine("Max Subjects reached!");
-                return;
-            }
-            if (numSubjects < MAX_SUBJECTS)
-            {
-                hWindow.Width += 300;
-                homeworkPanel.Width += 310;
-
-                windowGrid.Width = hWindow.Width;
-                double marginLeft = addSubjectButton.Margin.Left;
-                addSubjectButton.Margin = new Thickness(marginLeft += 300, 10, 0, 0);
-
-                homeworkPanel.Children.Add(createSubject());
-                numSubjects++;
-                totalSubjects++;
-            }
-            else
-            {
-                hWindow.Height += homeworkPanel.Height;
-                windowGrid.Height += homeworkPanel.Height;
-                homeworkPanel.Height += homeworkPanel.Height;
-                homeworkPanel.Orientation = System.Windows.Controls.Orientation.Vertical;
-                homeworkPanel.Children.Add(createSubject());
-                numSubjects = 0;
-                totalSubjects++;
-                homeworkPanel.Orientation = System.Windows.Controls.Orientation.Horizontal;
-            }
-
-        }
-
-        private void RichTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private Grid createSubject()
-        {
-            Grid newGrid = new Grid();
-            newGrid.Height = panelGrid.Height;
-            newGrid.Width = panelGrid.Width;
+            TabItem newTab = new TabItem();
+            newTab.Header = "Science";
+            readString = new StringReader(tabXAML);
+            reader = XmlReader.Create(readString);
+            ListView newList = (ListView)XamlReader.Load(reader);
             
-            Label newAssignmentLabel = new Label();
-            Label newDateLabel = new Label();
-            TextBox newSubjectBox = new TextBox();
-            ListView newList = new ListView();
-            newList.HorizontalAlignment = taskList.HorizontalAlignment;
-            newList.VerticalAlignment = taskList.VerticalAlignment;
-            newList.Width = taskList.Width;
-            newList.Height = taskList.Height;
-            newList.Margin = new Thickness(taskList.Margin.Left,taskList.Margin.Top, taskList.Margin.Right, taskList.Margin.Bottom);
-            newList.HorizontalContentAlignment = taskList.HorizontalContentAlignment;
-
-            newSubjectBox.Text = subjectBox.Text;
-            newSubjectBox.HorizontalAlignment = subjectBox.HorizontalAlignment;
-            newSubjectBox.VerticalAlignment = subjectBox.VerticalAlignment;
-            newSubjectBox.VerticalContentAlignment = subjectBox.VerticalContentAlignment;
-            newSubjectBox.Width = subjectBox.Width;
-            newSubjectBox.Height = subjectBox.Height;
-            newSubjectBox.HorizontalContentAlignment = subjectBox.HorizontalContentAlignment;
-            newSubjectBox.Margin = new Thickness(subjectBox.Margin.Left, subjectBox.Margin.Top, subjectBox.Margin.Right, subjectBox.Margin.Bottom);
-            
-            /*
-            if (leftBox.Document == null)
+            addButton = (Button) newList.FindName("deleteButton");
+            if (addButton != null)
             {
-                Debug.WriteLine("NULL");
+                Console.WriteLine("Is Not NULL WAHOO!!");
+                addButton.Click += new RoutedEventHandler(deleteTask);
             }
+            addButton = (Button) LogicalTreeHelper.FindLogicalNode(newList, "archiveButton") as Button;
+            if(addButton != null)
+                addButton.Click += new RoutedEventHandler(archiveTask);
             
-            leftBox.HorizontalAlignment = leftTextBox.HorizontalAlignment;
-            leftBox.VerticalAlignment = leftTextBox.VerticalAlignment;
-            leftBox.Height = leftTextBox.Height;
-            leftBox.Width = leftTextBox.Width;
-            leftBox.Margin = new Thickness(leftTextBox.Margin.Left,leftTextBox.Margin.Top,leftTextBox.Margin.Bottom,leftTextBox.Margin.Right);                                                
-
-            rightBox.HorizontalAlignment = rightTextBox.HorizontalAlignment;
-            rightBox.VerticalAlignment = rightTextBox.VerticalAlignment;
-            rightBox.Height = rightTextBox.Height;
-            rightBox.Width = rightTextBox.Width;
-            rightBox.Margin = new Thickness(rightTextBox.Margin.Left,rightTextBox.Margin.Top,rightTextBox.Margin.Bottom,rightTextBox.Margin.Right);                         
-            */
-            newGrid.Children.Add(newSubjectBox);
-            newGrid.Children.Add(newList);
-
-            /******* Adding logic for saving each subject into data structure *******/
-            /*
-            Subject newSubject = new Subject();
-            newSubject.subject = newSubjectBox;
-            newSubject.assignments = leftBox.Document;
-            newSubject.dates = rightBox.Document;
-
-            subjects[numSubjects - 1] = newSubject;
-            */
-            return newGrid;
+            taskCollection = new ObservableCollection<Task>();
+            newList.ItemsSource = taskCollection;
+            newTab.Content = newList;
+            subjectsTab.Items.Add(newTab);
+            reader.Close();
         }
 
         private void deleteTask(object sender, RoutedEventArgs e)
         {
+            Console.WriteLine("SJDBFGJSDGSD");
             Task item = (Task) (sender as Button).DataContext;
-            int index = taskList.Items.IndexOf(item);
-            taskCollection.Remove(item);
+            int index = ((ListView)((TabItem)subjectsTab.SelectedItem).Content).Items.IndexOf(item);
+            ((ObservableCollection<Task>)((ListView)((TabItem)subjectsTab.SelectedItem).Content).ItemsSource).Remove(item);
 
-            taskList.Items.Refresh();
+            ((ListView)((TabItem)subjectsTab.SelectedItem).Content).Items.Refresh();
            
+        }
+
+        private void archiveTask(object send, RoutedEventArgs e)
+        {
+            Console.WriteLine("??????????????????");
         }
 
         private void addTask(object sender, RoutedEventArgs e)
@@ -251,24 +196,15 @@ namespace Saveyour
                 return;
             }
             String description = setTaskWindow.getTaskDescription();
-            Console.WriteLine(description);
             String date = setTaskWindow.getTaskDate().ToShortDateString();
             String shortenedDate = date.Remove(date.Length - 5);
-            Console.WriteLine(date);
-            taskCollection.Add(new Task { AssignmentName = description, AssignmentDate = date, AssignmentShortenedDate = shortenedDate });
-            taskList.Items.Refresh();
+            TabItem setTab = subjectsTab.SelectedItem as TabItem;
+            ((ObservableCollection<Task>)((ListView)setTab.Content).ItemsSource).Add(new Task { AssignmentName = description, AssignmentDate = date, AssignmentShortenedDate = shortenedDate });
+            ((ListView)setTab.Content).Items.Refresh();
         }
 
-        private void leftTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
 
-        private void mouseEvent(object sender, MouseEventArgs e)
-        {
-            ListViewItem item = sender as ListViewItem;
-            Console.WriteLine(item);
-        }
 
     }
 }

e33432037e5e2341653207f175a86280eea22bf8
dengitzjg <dengitzjg@gmail.com>
2015-05-01 05:36:41 -0400

    Added a comment to IPlugin interface

--- Saveyour/PluginContracts/IPlugin.cs
+++ Saveyour/PluginContracts/IPlugin.cs
@@ -5,6 +5,10 @@ namespace PluginContracts
 	{
 		string Name { get; }
 		void Do();
+        
+        /*
+         * This function only needs to be implemented if the plugin has a Window to display. If it does not, simply return null.
+         * */
         Window getInstance();
 	}
 }
